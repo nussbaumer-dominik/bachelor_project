3651e0ef91ac86a53225f9f2823db52880e525b2
Dominik Nußbaumer <nussbaumer.dominik@pm.me>
2024-08-13 11:04:17 +0200

    add bulk import to postgres db to speed up import of very large graphs

--- benchmark/generate_graph.py
+++ benchmark/generate_graph.py
@@ -1,7 +1,6 @@
 import argparse
 import csv
 import os
-import random
 import subprocess
 import time
 
@@ -19,51 +18,21 @@ NEO4J_URI = os.getenv('NEO4J_URI')
 NEO4J_USER = os.getenv('NEO4J_USER')
 NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
 NEO4J_IMPORT_PATH = os.getenv('NEO4J_IMPORT_PATH')
+NEO4J_CONTAINER_NAME = "lsqb-neo"
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-CONTAINER_NAME = "lsqb-neo"
+POSTGRES_IMPORT_PATH = os.getenv("POSTGRES_IMPORT_PATH")
 
 
-# def generate_graph(num_nodes, avg_friendships):
-#     g = nx.Graph()
-#     g.add_nodes_from(range(num_nodes))
-#
-#     for node in g.nodes():
-#         num_edges = min(avg_friendships, num_nodes - 1)
-#         potential_friends = list(set(g.nodes()) - set(g.neighbors(node)) - {node})
-#         if num_edges > len(potential_friends):
-#             num_edges = len(potential_friends)
-#         friends = random.sample(potential_friends, num_edges)
-#         for friend in friends:
-#             g.add_edge(node, friend)
-#
-#     return g
-
-# def generate_graph(num_nodes, max_friendships):
-#     g = nx.Graph()
-#     g.add_nodes_from(range(num_nodes))
-#
-#     for node in g.nodes():
-#         num_edges = min(max_friendships, num_nodes - 1)  # Use min to ensure we don't exceed the max friendships
-#         potential_friends = list(set(g.nodes()) - set(g.neighbors(node)) - {node})
-#         if num_edges > len(potential_friends):
-#             num_edges = len(potential_friends)
-#         friends = random.sample(potential_friends, num_edges)
-#         for friend in friends:
-#             g.add_edge(node, friend)
-#
-#     return g
-
-def generate_regular_graph(n, degree):
-    g = nx.random_regular_graph(degree, n)
-    return g
+def generate_regular_graph(n: int, degree: int) -> nx.Graph:
+    return nx.random_regular_graph(degree, n)
 
 
 def restart_neo4j():
     try:
-        subprocess.run(f'docker restart {CONTAINER_NAME}', shell=True, check=True)
+        subprocess.run(f'docker restart {NEO4J_CONTAINER_NAME}', shell=True, check=True)
         print("Neo4j restarted successfully.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred during Neo4j restart: {e}")
@@ -71,7 +40,7 @@ def restart_neo4j():
 
 def import_graph_to_neo4j():
     command = (
-        f'docker exec {CONTAINER_NAME} neo4j-admin database import full --delimiter="," '
+        f'docker exec {NEO4J_CONTAINER_NAME} neo4j-admin database import full --delimiter="," '
         "--id-type=INTEGER "
         f"--nodes=Person=import/nodes.csv "
         f"--relationships=KNOWS=import/edges.csv "
@@ -85,21 +54,30 @@ def import_graph_to_neo4j():
         print(f"An error occurred during Neo4j import: {e}")
 
 
-def export_graph_to_csv(g):
-    with open(f'{NEO4J_IMPORT_PATH}/nodes.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["id:ID(Person)"])
-        for node in g.nodes():
-            writer.writerow([node])
+def export_graph_to_csv(g: nx.Graph, neo4j: bool, postgres: bool):
+    if neo4j:
+        with open(f'{NEO4J_IMPORT_PATH}/nodes.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["id:ID(Person)"])
+            for node in g.nodes():
+                writer.writerow([node])
 
-    with open(f'{NEO4J_IMPORT_PATH}/edges.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([":START_ID(Person)", ":END_ID(Person)"])
-        for edge in g.edges():
-            writer.writerow([edge[0], edge[1]])
+        with open(f'{NEO4J_IMPORT_PATH}/edges.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([":START_ID(Person)", ":END_ID(Person)"])
+            for edge in g.edges():
+                writer.writerow([edge[0], edge[1]])
+
+    if postgres:
+        with open(f'{POSTGRES_IMPORT_PATH}/edges.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["person1id", "person2id"])
+            for edge in g.edges():
+                writer.writerow([edge[0], edge[1]])
+                writer.writerow([edge[1], edge[0]])
 
 
-def create_graph_in_postgres(g):
+def create_graph_in_postgres(g: nx.Graph):
     postgres_conn = PostgreSQLConnection(POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD)
     try:
         with postgres_conn.conn.cursor() as cursor:
@@ -116,19 +94,40 @@ def create_graph_in_postgres(g):
         postgres_conn.close()
 
 
-def main(num_nodes, avg_friendships, neo4j, postgres):
+def bulk_import_to_postgres(g: nx.Graph, filepath, table_name: str):
+    postgres_conn = PostgreSQLConnection(POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD)
+    try:
+        with postgres_conn.conn.cursor() as cursor:
+            cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+            cursor.execute(f"CREATE TABLE {table_name} (person1id INT, person2id INT);")
+            postgres_conn.conn.commit()  # Commit the structural changes
+
+        with postgres_conn.conn.cursor() as cursor:
+            with cursor.copy(f"COPY {table_name} (person1id, person2id) FROM STDIN") as copy:
+                for edge in g.edges():
+                    copy.write_row(edge)  # Write edge
+                    copy.write_row((edge[1], edge[0]))  # Write reverse edge
+
+    except Exception as e:
+        print(f"An error occurred during PostgreSQL bulk import: {e}")
+    finally:
+        if postgres_conn:
+            postgres_conn.close()
+
+
+def main(num_nodes: int, avg_friendships: int, neo4j: bool, postgres: bool):
     graph_start_time = time.time()
-    #g = generate_graph(num_nodes, avg_friendships)
     g = generate_regular_graph(num_nodes, avg_friendships)
     print(f"Graph generated in {time.time() - graph_start_time:.2f} seconds")
-    print(f"Generated a graph with {num_nodes} nodes and {len(g.edges())} edges")
+    print(f"Generated a graph with {num_nodes:_} nodes and {len(g.edges()):_} edges")
+    export_graph_to_csv(g, neo4j, postgres)
     if neo4j:
-        export_graph_to_csv(g)
         import_graph_to_neo4j()
         print("Graph created in Neo4j database.")
 
     if postgres:
-        create_graph_in_postgres(g)
+        # create_graph_in_postgres(g)
+        bulk_import_to_postgres(g, f'{POSTGRES_IMPORT_PATH}/edges.csv', "Person_knows_Person")
         print("Graph created in PostgreSQL database.")
     print("Done!")
 
